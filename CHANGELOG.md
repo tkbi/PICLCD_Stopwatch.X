5d3b0c0debb0f4de2f473309564b696d081f60de
Nicolas Pannwitz <PIC-Nico@users.noreply.github.com>
2018-08-11 14:05:32 +0200

    Initial commit..
    This is the very first commit to create this repository, add some very basic files as well for Git and also for the MPLABX project.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+
+### Added
+- Initial Git-Repository
+- Empty main.c file
+- Empty main.h file
+
+### Changed
+### Removed
---
+++ main.c
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ *
+ * File:        main.c
+ * Project:     PICLCD-Stopwatch
+ * Author:      Nicolas Pannwitz (https://pic-projekte.de/)
+ * Comment:
+ * Licence:     Copyrightn (C) 2018 Nicolas Pannwitz
+ * 
+ *              This program is free software: You can redistribute it and/or 
+ *              modify it under the terms of the GNU General Public License as
+ *              published by the Free Software Foundation, either version 3 of
+ *              the License, or (at your option) any later version.
+ *
+ *              This program is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *              GNU General Public License for more details.
+ * 
+ *              You should have received a copy of the GNU General Public
+ *              License along with this program.
+ *              If not, see https://www.gnu.org/licenses/
+ * 
+ ******************************************************************************/
+
+#include <xc.h>
+#include "main.h"
+
+void main (void)
+{
+    while(1);
+}
